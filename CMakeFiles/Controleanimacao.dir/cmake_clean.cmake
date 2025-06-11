c4d225102d41d21284c2b136ca3d35869f2743a5
Leonardo Eugenio <leonardoeugenio977@gmail.com>
2025-06-10 23:08:59 -0300

    commit

---
+++ CMakeFiles/Controleanimacao.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Controleanimacao.dir/common/glad.c.obj"
+  "CMakeFiles/Controleanimacao.dir/common/glad.c.obj.d"
+  "CMakeFiles/Controleanimacao.dir/src/Modulo5/Controleanimacao.cpp.obj"
+  "CMakeFiles/Controleanimacao.dir/src/Modulo5/Controleanimacao.cpp.obj.d"
+  "Controleanimacao.exe"
+  "Controleanimacao.exe.manifest"
+  "Controleanimacao.pdb"
+  "libControleanimacao.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Controleanimacao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Controleanimacao.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Controleanimacao.
+# This may be replaced when dependencies are built.
